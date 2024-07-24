d0abfb5e71a572179561dd419fdbabeb1168f120
VikingVik123 <vodimmegwa@gmail.com>
2024-07-24 19:44:59 +0000

    Added task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+	const signUpUserPromise = signUpUser(firstName, lastName);
+	const uploadPhotoPromise = uploadPhoto(filename);
+	
+	return Promise.allSettled([signUpUserPromise, uploadPhotoPromise])
+		.then(results => {
+			const formattedResults = results.map(result => {
+				if (result.status === 'fulfilled') {
+					return { status: 'success', value: result.value };
+				} else {
+					return { status: 'error', reason: result.reason.message };
+				}
+			});
+			return formattedResults;
+		});
+}
