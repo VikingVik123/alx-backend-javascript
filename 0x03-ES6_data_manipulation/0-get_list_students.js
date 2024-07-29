aef464ea98fbf9989e76b9a0f6d0814f31d7ddbe
VikingVik123 <vodimmegwa@gmail.com>
2024-07-29 20:32:09 +0000

    Tried fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,7 @@
 export default function getListStudents() {
-  const student1 = [
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Fransico' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Fransico' },
   ];
-  return (student1);
 }
