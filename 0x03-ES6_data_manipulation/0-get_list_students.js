c0a8907d00cd75155264eecc8754a4855558c737
VikingVik123 <vodimmegwa@gmail.com>
2024-07-29 20:18:24 +0000

    Tried fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 export default function getListStudents() {
-	const student1 = [
-		{id : 1, firstName : "Guillaume", location : "San Fransico"},
-		{id : 2, firstName : "James", location : "Columbia"},
-		{id : 5, firstName : "Serena", location : "San Fransico"} 
-	];
-	return(student1);
+  const student1 = [
+    { id: 1, firstName: 'Guillaume', location: 'San Fransico' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Fransico' },
+  ];
+  return (student1);
 }
